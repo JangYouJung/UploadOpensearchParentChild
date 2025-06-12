2e37324554ed13e4e0130683a6786558880a8e4c
You Jung <80906691+JangYouJung@users.noreply.github.com>
2025-06-12 18:06:12 +0900

    chore: 서버 포트 변경 및 랭체인 버전 업그레이드

--- server.py
+++ server.py
@@ -17,4 +17,4 @@ api.add_resource(UploadToOpenSearch, '/api/opensearch/upload')
 
 if __name__ == "__main__":
     CORS(app)
-    app.run(debug=True, port=5000, host='0.0.0.0')
+    app.run(debug=True, port=5001, host='0.0.0.0')
